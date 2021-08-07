1eed918231d11fb286b4929f03c01dabdbaac612
PlaustralCL <plaustralcl@gmail.com>
2021-08-07 17:28:04 -0400

    Update hangman.rb with all the classes

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "./hangman/secret_word.rb"
+require_relative "./hangman/secret_word"
+require_relative "./hangman/cipher"
+require_relative "./hangman/player"
+require_relative "./hangman/game"
 
 # Hangman game
 module Hangman
