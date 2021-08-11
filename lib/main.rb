68400de3817c22f2d3d40cf5694ed1efef171692
PlaustralCL <plaustralcl@gmail.com>
2021-08-11 07:21:57 -0400

    Reformat welcome message

--- lib/main.rb
+++ lib/main.rb
@@ -4,6 +4,8 @@ require "yaml"
 require_relative "./hangman"
 
 puts "Welcome to hangman"
+puts "A word that is 5 - 12 letters long will be selected."
+puts "On each turn you can guess a letter. To win, you must solve the puzzle before you make 6 wrong guesses."
 puts ""
 
 secret_word = ""
@@ -11,7 +13,9 @@ cipher = ""
 player = ""
 game = Hangman::Setup.new
 
-puts "Do you want to (1) Load a saved game or (2) Start a new game?"
+puts "Do you want to:"
+puts "(1) Load a saved game"
+puts "(2) Start a new game\n\n"
 game_type = gets.chomp.to_i
 if game_type == 1
   secret_word, cipher, player = game.load_game
