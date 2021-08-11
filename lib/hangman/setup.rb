26f07bd16fdb8bf415081a94d1c64cfabae4bed9
PlaustralCL <plaustralcl@gmail.com>
2021-08-10 21:05:46 -0400

    Select saved game from list

---
+++ lib/hangman/setup.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "yaml"
+require_relative "../hangman"
+
+module Hangman
+  # Setup for a new game
+  class Setup
+    attr_reader :secret_word, :cipher, :player
+
+    def new_game
+      @secret_word = SecretWord.new("hangman")
+      @cipher = Cipher.new(secret_word.word)
+      @player = Player.new
+      [secret_word, cipher, player]
+    end
+
+    def load_game
+      entries = find_files
+      show_files(entries)
+      filename = "saved_games/#{choose_file(entries)}"
+      # filename = "saved_games/#{filename}"
+      read_file(filename)
+      [secret_word, cipher, player]
+    end
+
+    def choose_file(entries)
+      puts "Please select a file"
+      file = gets.chomp.to_i
+      entries[file]
+    end
+
+    def read_file(filename)
+      game_state = YAML.load(File.read(filename))
+      @secret_word = SecretWord.new(game_state[:secret_word])
+      @cipher = Cipher.new(game_state[:secret_word], game_state[:cipher])
+      @player = Player.new(game_state[:wrong_guesses])
+    end
+
+    def find_files
+      Dir.entries("saved_games").sort[2..-1]
+    end
+
+    def show_files(entries)
+      entries.each_with_index do |file, index|
+        puts "(#{index})   #{file}"
+      end
+    end
+
+
+  end
+end
