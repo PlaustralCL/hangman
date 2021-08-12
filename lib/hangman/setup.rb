a51481ee3a4b496ff5ac61e00aa7d4eaab07e9bf
PlaustralCL <plaustralcl@gmail.com>
2021-08-11 20:10:35 -0400

    Move all support from main to Setup

--- lib/hangman/setup.rb
+++ lib/hangman/setup.rb
@@ -8,20 +8,49 @@ module Hangman
   class Setup
     attr_reader :secret_word, :cipher, :player
 
+    def initialize
+      welcome
+      choose_game_type
+      game = Game.new(secret_word, cipher, player)
+      game.play
+      game_end
+    end
+
+    def game_end
+      puts "You guessed the word!" if cipher.solved?
+      puts "Better luck next time. The secret word was #{secret_word.word}." unless cipher.solved?
+    end
+
+    def choose_game_type
+      options = <<~HEREDOC
+        Do you want to:
+        (1) Load a saved game
+        (2) Start a new game\n\n
+      HEREDOC
+      puts options
+
+      game_type = gets.chomp.to_i
+      if game_type == 1
+        saved_game
+      else
+        new_game
+      end
+    end
+
     def new_game
       @secret_word = SecretWord.new("hangman")
       @cipher = Cipher.new(secret_word.word)
       @player = Player.new
-      [secret_word, cipher, player]
+      # [secret_word, cipher, player]
     end
 
-    def load_game
+    def saved_game
       entries = find_files
       show_files(entries)
       filename = "saved_games/#{choose_file(entries)}"
       # filename = "saved_games/#{filename}"
       read_file(filename)
-      [secret_word, cipher, player]
+      # [secret_word, cipher, player]
     end
 
     def choose_file(entries)
@@ -48,6 +77,16 @@ module Hangman
       end
     end
 
+    def welcome
+      intro = <<~HEREDOC
+        Welcome to hangman
+        A word that is 5 - 12 letters long will be selected.
+        On each turn you can guess a letter. To win, you must solve the puzzle
+        before you make 6 wrong guesses.
+      HEREDOC
 
+      puts intro
+      puts ""
+    end
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,35 +1,5 @@
 # frozen_string_literal: true
 
-require "yaml"
 require_relative "./hangman"
 
-puts "Welcome to hangman"
-puts "A word that is 5 - 12 letters long will be selected."
-puts "On each turn you can guess a letter. To win, you must solve the puzzle before you make 6 wrong guesses."
-puts ""
-
-secret_word = ""
-cipher = ""
-player = ""
-game = Hangman::Setup.new
-
-puts "Do you want to:"
-puts "(1) Load a saved game"
-puts "(2) Start a new game\n\n"
-game_type = gets.chomp.to_i
-if game_type == 1
-  secret_word, cipher, player = game.load_game
-else
-  secret_word, cipher, player = game.new_game
-end
-
-puts ""
-game = Hangman::Game.new(secret_word, cipher, player)
-
-game.play
-
-
-
-
-puts "You guessed the word!" if cipher.solved?
-puts "Better luck next time. The secret word was #{secret_word.word}." unless cipher.solved?
+Hangman::Setup.new
